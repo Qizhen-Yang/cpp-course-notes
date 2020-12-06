8cd868452bbb2ab4ea0d452b98aa0865e87d335f
Qizhen-Yang <68524956+Qizhen-Yang@users.noreply.github.com>
2020-12-06 12:10:40 +0800

    Update 分数变小数.cpp

--- 分数变小数/分数变小数.cpp
+++ 分数变小数/分数变小数.cpp
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
 int pos[100000];
-// pos[r]表示 r这个数
+// pos[r] 表示 r 这个数
 // 第一次出现在余数序列的哪个位置
 
 
 int main()
 {
 	int p, q;							// 分子，分母
-	cin >> p >> q;
-	cout << p/q;
-	int r = p % q;
-	if (r == 0)
-		return 0;
-	cout << ".";  
-	int step = 1;
-	while (r != 0 && pos[r] == 0)
+	scanf("%d%d", &p, &q);				// 输入
+	printf("%d", p / q);				// 输出除法得到的整数部分
+
+	int r = p % q;						// 取余
+	if (r == 0)							// 如果余数为0
+		return 0;						// 结束
+
+	printf(".");						// 输出小数点
+	int step = 1;						// 小数点后的数位
+
+	while (r != 0 && pos[r] == 0)		// 
 	{
 		pos[r] = step;	  	
 		r = r * 10 % q;
@@ -25,5 +28,25 @@ int main()
 	{
 		int left = pos[r];
 		int r = p%q;
+		for (int i = 1; i < left; ++i)
+		{
+			cout << 10*r/q;
+			r = 10*r%q;
+		}
+	cout << "(";
+	for (int i = left; i < step; ++i)
+	{
+		cout << 10*r/q;
+		r = 10*r%q;
+	}
+	cout << ")";
+	}
+	else
+	{
+		for (int i = 1; i < step; ++i)
+		{
+			cout << 10*r/q;
+			r = 10*r%q;
+		}
 	}
 }
